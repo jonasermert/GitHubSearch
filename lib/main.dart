14fbee7e47fb29ffe797c7af7553111bd8ea163c
Jonas Ermert <ermert.jonas@yahoo.de>
2023-05-27 13:32:59 +0200

    27.05.2023
    added  images and theme

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,44 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'GitHub Search',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8.0),
+                    child: Image.asset(
+                      'images/github.icons8.png',
+                      width: 24,
+                      height: 24,
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: GitHubSearchPage(),
+              ),
+              Container(
+                color: Colors.blue,
+                padding: const EdgeInsets.all(8.0),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Copyright 2023 Jonas Ermert',
+                    style: TextStyle(
+                      fontSize: 10,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      home: GitHubSearchPage(),
     );
   }
 }
